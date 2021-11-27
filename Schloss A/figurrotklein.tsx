6c913875c72107ccbd06affdc1866aa1b3a63ab4
Timo1922 <84066180+Timo1922@users.noreply.github.com>
2021-11-27 16:07:06 +0100

    Delete figurrotklein.tsx

--- Schloss A/figurrotklein.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="figurrotklein" tilewidth="32" tileheight="32" margin="5" tilecount="805" columns="23">
- <image source="figurrotklein.png" width="743" height="1128"/>
-</tileset>
