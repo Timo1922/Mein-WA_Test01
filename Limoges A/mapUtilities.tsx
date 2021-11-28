a82a9b6c6b503365d0d399e79b686d4cd606af59
Timo1922 <84066180+Timo1922@users.noreply.github.com>
2021-11-28 19:43:15 +0100

    Add files via upload

---
+++ Limoges A/mapUtilities.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="mapUtilities" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="9" columns="9">
+ <image source="../Tuilerien A/mapUtilities.png" trans="ff00ff" width="306" height="34"/>
+ <tile id="3">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
