18f79ba752e3746b652c4d29fab6e7b6afdae2be
Timo1922 <84066180+Timo1922@users.noreply.github.com>
2021-12-17 22:19:26 +0100

    Delete tilesets_deviant_milkian.tsx

--- Ballett Kuecklein A/tilesets_deviant_milkian.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="tilesets_deviant_milkian" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../Katakomben &amp; Cum Mortuis A/tilesets_deviant_milkian.png" width="512" height="512"/>
-</tileset>
