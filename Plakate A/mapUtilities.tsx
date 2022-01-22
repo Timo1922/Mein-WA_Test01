485889289b2ea4b7aaabdc4501c691eb5a1f9d85
Timo1922 <84066180+Timo1922@users.noreply.github.com>
2022-01-22 14:12:59 +0100

    Add files via upload

---
+++ Plakate A/mapUtilities.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="mapUtilities" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="9" columns="9">
+ <properties>
+  <property name="tilesetCopyright" value="o&#9;mapUtilities, URL: https://github.com/workadventure-xce/workadventure-xce-map-starter-kit/blob/main/mapUtilities.png [abgerufen am 10.12.2021]."/>
+ </properties>
+ <image source="../Ballett Kuecklein A/mapUtilities.png" trans="ff00ff" width="306" height="34"/>
+ <tile id="3">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
