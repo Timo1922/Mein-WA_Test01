60e7de46f742e4bf76e79dc3edaf9912cfe87fa6
Timo1922 <84066180+Timo1922@users.noreply.github.com>
2022-02-07 11:56:52 +0100

    Add files via upload

--- Blauer Salon A/mapUtilities.tsx
+++ Blauer Salon A/mapUtilities.tsx
@@ -1,6 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="mapUtilities" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="9" columns="9">
- <image source="Bilder Einer Ausstellung WA/Räume_Kommilitonen/Vorlagen Paul Lemke/mapUtilities.png" trans="ff00ff" width="306" height="34"/>
+ <properties>
+  <property name="tilesetCopyright" value="o&#9;mapUtilities (CC BY-SA-NC-ND), erstellt von Timo Miederhoff – in Anlehnung an mapUtilities, URL: https://github.com/workadventure-xce/workadventure-xce-map-starter-kit/blob/main/mapUtilities.png [abgerufen am 10.12.2021]."/>
+ </properties>
+ <image source="../Ballett Kuecklein A/mapUtilities.png" trans="ff00ff" width="306" height="34"/>
+ <tile id="0">
+  <properties>
+   <property name="tilesetCopyright" value="o&#9;mapUtilities (Urheber vmtl. aus der chaos-Blase), URL: https://github.com/workadventure-xce/workadventure-xce-map-starter-kit/blob/main/mapUtilities.png [abgerufen am 10.12.2021]."/>
+  </properties>
+ </tile>
  <tile id="3">
   <properties>
    <property name="collides" type="bool" value="true"/>
